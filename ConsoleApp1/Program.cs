8d8893c80ef0c2974efd0c74ab03ebaba5791e3e
Даниил <puiuld47@gmail.com>
2024-12-02 18:45:53 +0200

    Task5

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// вычисление
+    /// </summary>
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double y = Fune(7) + Fune(11) + Fune(15);
+            Console.WriteLine($"y={y}");
+            Console.ReadKey();
+        }
+        public static double Fune(int x)
+        {
+            return (Math.Sqrt(x) + x / 3);
+        }
+    }
+}
